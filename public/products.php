f2b53539cc73f8c312a643e181fae200fef15149
Oleh <berolegnik@gmail.com>
2025-03-02 11:59:21 +0200

    added style

--- public/products.php
+++ public/products.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Главная страница</title>
     <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
-    <!-- <link rel="stylesheet" type="text/css" href="../assets/css/products.css"> -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/products.css">
 </head>
 <body>
     
